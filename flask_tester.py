eb5b367d83979bc293fefcf41a358ea216e4c2bb
lokranjanp <lokranjan03@gmail.com>
2024-12-23 09:08:01 +0530

    set params for new locust tests

--- flask_tester.py
+++ flask_tester.py
@@ -1,11 +1,26 @@
 import requests
+import random
+import csv
 from register import hash_password
 import time
 
-url = "https://127.0.0.1:7019/register"
-data = {"numrows": 10}
-start = time.time()
-response = requests.post(url, json=data, verify='/Users/lokranjan/PycharmProjects/workloadsim/cert.pem')
-end = time.time()
-print(end-start)
-print(response.json())
+def load_usernames_from_csv(filepath):
+    with open(filepath, "r") as file:
+        reader = csv.DictReader(file)
+        return [row["username"] for row in reader]
+
+USERNAMES = load_usernames_from_csv("userdump.csv")
+
+iter = 0
+
+while iter<1:
+    random.seed(time.time())
+    url = "https://127.0.0.1:7019/otp"
+    data = {"username": random.choice(USERNAMES)}
+    print(data)
+    start = time.time()
+    response = requests.post(url, json=data, verify='/Users/lokranjan/PycharmProjects/workloadsim/cert.pem')
+    end = time.time()
+    print(iter, end-start)
+    iter += 1
+
