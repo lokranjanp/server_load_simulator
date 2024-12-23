08c8a73b4aefc8649a4be75375a84700827d60fb
lokranjanp <lokranjan03@gmail.com>
2024-12-23 18:13:35 +0530

    add request test func + task dec weight

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,5 @@
 from locust import HttpUser, task, between
 import time
-import random
-from register import hash_password
 import csv
 import random
 
@@ -15,26 +13,61 @@ USERNAMES = load_usernames_from_csv("userdump.csv")
 class SimulateFlaskApp(HttpUser):
     wait_time = between(1, 3)
     host = "https://127.0.0.1:7019"  # Add your host here
+    random.seed(time.time())
 
-    def on_start(self):
-        random.seed(time.time())
-        self.cert_path = "/Users/lokranjan/PycharmProjects/workloadsim/cert.pem"
-        self.username = random.choice(USERNAMES)
-
-    @task
+    @task(1)
     def test_otpgen(self):
         random.seed(time.time())
-        data = {"username": self.username}
+        data = {"username": random.choice(USERNAMES)}
+        cert_path = "/Users/lokranjan/PycharmProjects/workloadsim/cert.pem"
         start_time = time.time()
         with self.client.post(
             "/otp",  # Endpoint relative to the host
             json=data,
-            verify=self.cert_path,
+            verify=cert_path,
             catch_response=True
         ) as response:
             end_time = time.time()
             if response.status_code == 200:
                 response.success()
-                print(f"Request took {end_time - start_time:.2f} seconds")
+                print(f"Serve OTP Request took {end_time - start_time:.2f} seconds")
             else:
                 response.failure(f"Failed with status code {response.status_code}: {response.text}")
+
+    @task(3)
+    def test_login(self):
+        random.seed(time.time())
+        cert_path = "/Users/lokranjan/PycharmProjects/workloadsim/cert.pem"
+        self.username = random.choice(USERNAMES)
+        data = {"username": self.username, "password":self.username}
+        start_time = time.time()
+        with self.client.post(
+            "/login", json=data,
+            verify=cert_path,
+            catch_response=True
+        ) as response:
+            end_time = time.time()
+            if response.status_code == 200:
+                response.success()
+                print(f"Login Request took {end_time - start_time:.2f} seconds")
+            else:
+                response.failure(f"Failed with status code {response.status_code}: {response.text}")
+
+    @task(1)
+    def test_register(self):
+        random.seed(time.time())
+        cert_path = "/Users/lokranjan/PycharmProjects/workloadsim/cert.pem"
+        data = {"numrows": 1}
+        start_time = time.time()
+        with self.client.post(
+            "/register", json=data,
+            verify=cert_path,
+            catch_response=True
+        ) as response:
+            end_time = time.time()
+            if response.status_code == 201:
+                response.success()
+                print(f"Register Request took {end_time - start_time:.2f} seconds")
+            else:
+                response.failure(f"Failed with status code {response.status_code}: {response.text}")
+
