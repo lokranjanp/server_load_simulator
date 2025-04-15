768f164cc106eb9d913bfcd8424eb669296c779e
lokranjanp <lokranjan03@gmail.com>
2025-04-15 19:57:40 +0530

    final review changes

--- locustfile.py
+++ locustfile.py
@@ -2,6 +2,10 @@ from locust import HttpUser, task, between
 import time
 import csv
 import random
+import csv
+import time
+import psutil
+import os
 
 def load_usernames_from_csv(filepath):
     with open(filepath, "r") as file:
@@ -10,6 +14,32 @@ def load_usernames_from_csv(filepath):
 
 USERNAMES = load_usernames_from_csv("usernames.csv")
 
+
+# File to store the metrics
+csv_file = "server_metrics_local.csv"
+
+# Write headers to CSV
+with open(csv_file, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["timestamp", "cpu_percent", "memory_percent", "load_avg", "active_connections"])
+
+# Function to log metrics
+def log_metrics():
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory_percent = psutil.virtual_memory().percent
+    load_avg = os.getloadavg()[0]
+    active_connections = len(psutil.net_connections(kind='inet'))
+
+    try:
+        with open(csv_file, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([timestamp, cpu_percent, memory_percent, load_avg, active_connections])
+        print(f"[{timestamp}] Logged metrics to {csv_file}")
+    except Exception as e:
+        print(f"[Error] Failed to write to CSV: {e}")
+
+
 class SimulateFlaskApp(HttpUser):
     wait_time = between(1, 3)
     host = "http://0.0.0.0:7019"  # Add your host here
@@ -17,6 +47,7 @@ class SimulateFlaskApp(HttpUser):
 
     @task(1)
     def test_otpgen(self):
+        log_metrics()
         random.seed(time.time())
         data = {"username": random.choice(USERNAMES)}
         start_time = time.time()
@@ -35,6 +66,7 @@ class SimulateFlaskApp(HttpUser):
 
     @task(3)
     def test_login(self):
+        log_metrics()
         random.seed(time.time())
         self.username = random.choice(USERNAMES)
         data = {"username": self.username, "password":self.username}
@@ -52,6 +84,7 @@ class SimulateFlaskApp(HttpUser):
 
     @task(1)
     def test_register(self):
+        log_metrics()
         random.seed(time.time())
         data = {"numrows": 1}
         start_time = time.time()
@@ -65,4 +98,3 @@ class SimulateFlaskApp(HttpUser):
                 print(f"Register Request took {end_time - start_time:.2f} seconds")
             else:
                 response.failure(f"Failed with status code {response.status_code}: {response.text}")
-
